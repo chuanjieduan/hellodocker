45a8be3a7809c8dcf35a23fb4a326f392bb83b6a
chuanjie.duan <chuanjie.duan@envisioncn.com>
2018-07-18 23:09:37 +0800

    update

--- src/main/java/com/envisioncn/hellodocker/Application.java
+++ src/main/java/com/envisioncn/hellodocker/Application.java
@@ -5,12 +5,22 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @SpringBootApplication
 @RestController
 public class Application {
+	
+	private static Logger LOG = LoggerFactory.getLogger("tasklogger");
+	
+	private final static AtomicInteger counter = new AtomicInteger(1);
 
 	@RequestMapping("/")
 	public String home() {
+		LOG.info("current access count is: " + counter.getAndIncrement());
 		return "Hello Docker World";
 	}
 
